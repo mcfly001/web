72d1885a811525085bc18ade8194b0ff08657f3a
douchi <douchi@2dfire.com>
2019-01-16 21:18:55 +0800

    ad

--- src/components/LeftNav.vue
+++ src/components/LeftNav.vue
@@ -35,7 +35,6 @@
             :key="item.index"
             :index="item.index"
           >
-            <i :class="item.icon"></i>
             <span slot="title">{{item.name}}</span>
           </el-menu-item>
         </el-menu-item-group>
@@ -64,43 +63,36 @@ export default {
           name: "网络配置",
           children: [
             {
-              icon: "el-icon-m",
               index: "2-1",
               name: "工作模式",
               path: "/htmls/workMode"
             },
             {
-              icon: "el-icon-m",
               index: "2-2",
               name: "外网配置",
               path: "/htmls/wanSet"
             },
             {
-              icon: "el-icon-m",
               index: "2-3",
               name: "内网配置",
               path: "/htmls/lanSet"
             },
             {
-              icon: "el-icon-m",
               index: "2-4",
               name: "DHCP服务",
               path: "/htmls/dhcpSet"
             },
             {
-              icon: "el-icon-m",
               index: "2-5",
               name: "VLAN配置",
               path: "/htmls/vlanInf"
             },
             {
-              icon: "el-icon-m",
               index: "2-6",
               name: "端口映射",
               path: "/htmls/virtualServer"
             },
             {
-              icon: "el-icon-m",
               index: "2-7",
               name: "动态域名",
               path: "/htmls/wlan"
@@ -113,13 +105,11 @@ export default {
           name: "无线配置",
           children: [
             {
-              icon: "el-icon-m",
               index: "3-1",
               name: "网络名称",
               path: "/htmls/wlan"
             },
             {
-              icon: "el-icon-m",
               index: "3-2",
               name: "射频配置",
               path: "/htmls/radio"
@@ -132,13 +122,11 @@ export default {
           name: "用户管理",
           children: [
             {
-              icon: "el-icon-m",
               index: "4-1",
               name: "用户状态",
               path: "/htmls/stalist"
             },
             {
-              icon: "el-icon-m",
               index: "4-2",
               name: "黑名单",
               path: "/htmls/blacklist"
@@ -151,25 +139,21 @@ export default {
           name: "系统配置",
           children: [
             {
-              icon: "el-icon-m",
               index: "5-1",
               name: "网管策略",
               path: "/htmls/managementStrategy"
             },
             {
-              icon: "el-icon-m",
               index: "5-2",
               name: "时钟管理",
               path: "/htmls/basicSet"
             },
             {
-              icon: "el-icon-m",
               index: "5-3",
               name: "系统维护",
               path: "/htmls/systemMaintenance"
             },
             {
-              icon: "el-icon-m",
               index: "5-4",
               name: "计划任务",
               path: "/htmls/workMode"
@@ -182,25 +166,21 @@ export default {
           name: "引导",
           children: [
             {
-              icon: "el-icon-m",
               index: "6-1",
               name: "密码修改",
               path: "/htmls/workMode"
             },
             {
-              icon: "el-icon-m",
               index: "6-2",
               name: "工作模式",
               path: "/htmls/workMode"
             },
             {
-              icon: "el-icon-m",
               index: "6-3",
               name: "外网配置",
               path: "/htmls/workMode"
             },
             {
-              icon: "el-icon-m",
               index: "6-4",
               name: "无线配置",
               path: "/htmls/workMode"
@@ -232,5 +212,9 @@ export default {
   font-size: 20px;
   margin-right: 6px;
   font-weight: bold;
+}
+
+.el-menu-item{
+  padding-left: 50px !important;
 }
 </style>
